6fa2f875cc1d811e91332f64b8b9641cfcc1a088
Alessandro Mariani <amariani@expedia.com>
2015-08-16 14:51:10 +0100

    adding timing and better printed precision

--- beat_withrf.py
+++ beat_withrf.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import ensemble, preprocessing, cross_validation
 from sklearn.metrics import roc_auc_score as auc
+from time import time
 
 # PREPARE DATA
 data = pd.read_csv('data/train.csv').set_index("ID")
@@ -33,12 +34,13 @@ rf = ensemble.RandomForestClassifier(n_jobs=4, n_estimators = 20, random_state =
 
 # CROSS VALIDATE AND PRINT TRAIN AND TEST SCORE
 kf = cross_validation.StratifiedKFold(y, n_folds=3, shuffle=True, random_state=11)
-trscores, cvscores = [], []
+trscores, cvscores, times = [], [], []
 for itr, icv in kf:
+    t = time()
     trscore = auc(y.iloc[itr], rf.fit(X.iloc[itr], y.iloc[itr]).predict_proba(X.iloc[itr])[:,1])
     cvscore = auc(y.iloc[icv], rf.predict_proba(X.iloc[icv])[:,1])
-    trscores.append(trscore), cvscores.append(cvscore)
-print "TRAIN %.2f | TEST %.2f" % (np.mean(trscores), np.mean(cvscores))
+    trscores.append(trscore); cvscores.append(cvscore); times.append(time()-t)
+print "TRAIN %.4f | TEST %.4f | TIME %.2fm (1-fold)" % (np.mean(trscores), np.mean(cvscores), np.mean(times)/60)
 
 # MAKING SUBMISSION
 submission = pd.DataFrame(rf.fit(X,y).predict_proba(test.fillna(0))[:,1], index=test.index, columns=['target'])
